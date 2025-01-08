aa52d27da7358c54bac5cbdf6c8f3ac1e0ef0a36
Daria_Burlaka <daria.babakova@gmail.com>
2025-01-08 20:13:52 +0200

    Use the Heroshot component in the app/(pages)/(HomePage)/page component.

--- app/(pages)/(HomePage)/page.tsx
+++ app/(pages)/(HomePage)/page.tsx
@@ -1,21 +1,12 @@
+// Components
+import { Heroshot } from '@/app/(pages)/(HomePage)/components';
 // Styles
 import styles from './HomePage.module.scss';
 
 export default function Home() {
   return (
     <div className={styles.root}>
-      <h1 className={styles.title}>Boost your rankings with AI.</h1>
-
-      <p className={styles.description}>
-        Elevate your site’s visibility effortlessly with AI, where smart
-        technology meets user-friendly SEO tools.
-      </p>
-
-      <div className={styles.ctaWrapper}>
-        <button className={styles.cta}>
-          Start for free
-        </button>
-      </div>
+      <Heroshot />
     </div>
   );
 }
