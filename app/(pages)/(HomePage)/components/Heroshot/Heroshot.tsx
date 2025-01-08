0fe1de5a160d5c2dbcbef6f0f0c62c843cf84039
Daria_Burlaka <daria.babakova@gmail.com>
2025-01-08 20:12:58 +0200

    Create the app/(pages)/(HomePage)/components/Heroshot/Heroshot component.

---
+++ app/(pages)/(HomePage)/components/Heroshot/Heroshot.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image';
+// UI
+import {
+  Button,
+  ButtonColor,
+  UIComponentSize,
+} from '@/app/ui';
+// Images
+import HeroshotIllustration from '@/app/(pages)/(HomePage)/components/Heroshot/heroshot-illustration.png';
+// Styles
+import styles from './Heroshot.module.scss';
+
+export const Heroshot = () => {
+  return (
+    <section className={styles.root}>
+      <div>
+        <p className={styles.badge}>
+          <i className={styles.badgeType}>
+            NEW
+          </i>
+          {' '}
+          <span className={styles.badgeLabel}>
+          Latest integration just arrived
+        </span>
+        </p>
+
+        <div className={styles.infoWrapper}>
+          <h1 className={styles.infoTitle}>Boost your rankings with AI.</h1>
+
+          <p className={styles.infoDescription}>
+            Elevate your site’s visibility effortlessly with AI, where smart
+            technology meets user-friendly SEO tools.
+          </p>
+
+          <Button
+            color={ButtonColor.White}
+            size={UIComponentSize.Large}
+            outerBorder
+          >
+            Start for free
+          </Button>
+        </div>
+      </div>
+
+      <div className={styles.illustrationWrapper}>
+        <Image
+          className={styles.illustration}
+          src={HeroshotIllustration}
+          alt="Heroshot illustration"
+          aria-hidden={true}
+          loading="lazy"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Heroshot;
