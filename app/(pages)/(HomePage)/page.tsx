084d1364f500ed11fe0c3244cc1fc57277fd97ae
Daria_Burlaka <daria.babakova@gmail.com>
2025-01-14 17:53:18 +0200

    Use the Pricing component in the app/(pages)/(HomePage)/page component.

--- app/(pages)/(HomePage)/page.tsx
+++ app/(pages)/(HomePage)/page.tsx
@@ -1,5 +1,12 @@
 // Components
-import { Heroshot, Companies, Bento, Features, Clients } from '@/app/(pages)/(HomePage)/components';
+import {
+  Heroshot,
+  Companies,
+  Bento,
+  Features,
+  Clients,
+  Pricing
+} from '@/app/(pages)/(HomePage)/components';
 // Styles
 import styles from './HomePage.module.scss';
 
@@ -11,6 +18,7 @@ export default function Home() {
       <Bento />
       <Features />
       <Clients />
+      <Pricing/>
     </div>
   );
 }
